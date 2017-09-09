f0d0808b4e97b9d72c59a4cb121e14fdc00811bf
Chyld Medford <chyld.medford@gmail.com>
2017-09-09 15:06:19 -0700

    goroutines

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"runtime"
+	"sync"
 )
 
 type dog struct {
@@ -10,17 +12,41 @@ type dog struct {
 }
 
 var x int
+var wg = sync.WaitGroup{}
 
 func main() {
-	d := dog{name: "fido", age: 3}
-	da := &d
-	da.bark()
-	da.sit()
-	fmt.Printf("dog is %#v \n", d)
-	var a int32 = 3
-	fmt.Printf("a is %#v and type %T \n", a, a)
-	fmt.Printf("fib of 10 is %#v \n", fib(10))
-	fizzbuzz(20)
+	threads := runtime.GOMAXPROCS(-1)
+	fmt.Printf("threads: %#v\n", threads)
+
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		cpu()
+	}
+
+	wg.Wait()
+
+	// d := dog{name: "fido", age: 3}
+	// da := &d
+	// da.bark()
+	// da.sit()
+	// fmt.Printf("dog is %#v \n", d)
+	// var a int32 = 3
+	// fmt.Printf("a is %#v and type %T \n", a, a)
+	// fmt.Printf("fib of 10 is %#v \n", fib(10))
+	// fizzbuzz(20)
+}
+
+func cpu() {
+	var x int
+	for i := 0; i < 1000; i++ {
+		for j := 0; j < 1000; j++ {
+			for k := 0; k < 1000; k++ {
+				x = i * j * k
+			}
+		}
+	}
+	fmt.Println(x)
+	wg.Done()
 }
 
 func (d dog) bark() {
